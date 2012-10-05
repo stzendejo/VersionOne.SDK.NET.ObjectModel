86e9c933d11e556f5c23a4b49f6d4d3849ea9414
jogoshugh <jgough@JGough.corp.versionone.net>
2012-10-05 19:05:07 -0400

    Killed dead bat files...

--- buildObjectModel.Tests.bat
+++
@@ -1,6 +0,0 @@
-mkdir packages
-cd ObjectModel.Tests
-call ..\..\GetBuildTools\bin\NuGet\RestorePackagesOnly.bat VersionOne.SDK.ObjectModel.Tests.csproj
-call ..\..\GetBuildTools\bin\NuGet\UpdatePackages.bat
-cd ..\
-buildObjectModel.Tests.MSBuild.bat
--- buildObjectModel.bat
+++
@@ -1,6 +0,0 @@
-mkdir packages
-cd ObjectModel
-call ..\..\GetBuildTools\bin\NuGet\RestorePackagesOnly.bat VersionOne.SDK.ObjectModel.csproj
-call ..\..\GetBuildTools\bin\NuGet\UpdatePackages.bat
-cd ..\
-buildObjectModel.MSBuild.bat
