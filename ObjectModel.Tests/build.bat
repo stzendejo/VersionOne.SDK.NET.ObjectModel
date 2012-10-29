894ef20d925b408ff46984d9cf8732d5500cf4d7
jogoshugh <jgough@JGough.corp.versionone.net>
2012-10-29 19:23:45 -0400

    Updated for public release

--- ObjectModel.Tests/build.bat
+++ ObjectModel.Tests/build.bat
@@ -1,4 +1,19 @@
 mkdir ..\packages
 call NuGetRestorePackagesOnly.bat VersionOne.SDK.ObjectModel.Tests.csproj %1
-call NuGetUpdatePackages.bat packages.config
-msbuild VersionOne.SDK.ObjectModel.Tests.csproj /p:NuGetExePath=%1\NuGet.exe /p:RequireRestoreConsent=false /p:Configuration=Release /p:Platform=AnyCPU /p:Major=12 /p:Minor=2  /p:Revision=0 /p:AssemblyInformationalVersion="12.2.1.3588 Summer 2012" /p:AssemblyCopyright="Copyright 2012, VersionOne, Inc. Licensed under modified BSD." /p:CompanyName="VersionOne, Inc" /p:AssemblyProduct="VersionOne.SDK.ObjectModel.Tests" /p:AssemblyTitle="VersionOne SDK Object Model Tests" /p:AssemblyDescription="VersionOne SDK .NET API Object Model Tests Release Build"
+call NuGetUpdatePackages.bat packages.config 
+msbuild VersionOne.SDK.ObjectModel.Tests.csproj /p:V1BuildToolsPath=%1 ^
+/p:NuGetExePath=%1\NuGet.exe ^
+/p:RequireRestoreConsent=false ^
+/p:Configuration=Release ^
+/p:Platform=AnyCPU ^
+/p:Major=12 ^
+/p:Minor=3 ^
+/p:Revision=0 ^
+/p:AssemblyInformationalVersion="See https://github.com/versionone/VersionOne.SDK.NET.ObjectModel/wiki" ^
+/p:AssemblyCopyright="Copyright 2012, VersionOne, Inc. Licensed under modified BSD." ^
+/p:CompanyName="VersionOne, Inc" ^
+/p:AssemblyProduct="VersionOne.SDK.ObjectModel.Tests" ^
+/p:AssemblyTitle="VersionOne SDK Object Model Tests" ^
+/p:AssemblyDescription="VersionOne SDK .NET Object Model Tests Release Build" ^
+/p:SignAssembly=%SIGN_ASSEMBLY% ^
+/p:AssemblyOriginatorKeyFile=%SIGNING_KEY%
--- ObjectModel/build.bat
+++ ObjectModel/build.bat
@@ -1,4 +1,19 @@
 mkdir ..\packages
 call NuGetRestorePackagesOnly.bat VersionOne.SDK.ObjectModel.csproj %1
-call NuGetUpdatePackages.bat packages.config
-msbuild VersionOne.SDK.ObjectModel.csproj /p:V1BuildToolsPath=%1 /p:NuGetExePath=%1\NuGet.exe /p:RequireRestoreConsent=false /p:Configuration=Release /p:Platform=AnyCPU /p:Major=12 /p:Minor=2  /p:Revision=0 /p:AssemblyInformationalVersion="12.2.1.3588 Summer 2012" /p:AssemblyCopyright="Copyright 2012, VersionOne, Inc. Licensed under modified BSD." /p:CompanyName="VersionOne, Inc" /p:AssemblyProduct="VersionOne.SDK.ObjectModel" /p:AssemblyTitle="VersionOne SDK Object Model" /p:AssemblyDescription="VersionOne SDK .NET Object Model Release Build" /p:SignAssembly=%SIGN_ASSEMBLY% /p:AssemblyOriginatorKeyFile=%SIGNING_KEY%
+call NuGetUpdatePackages.bat packages.config 
+msbuild VersionOne.SDK.ObjectModel.csproj /p:V1BuildToolsPath=%1 ^
+/p:NuGetExePath=%1\NuGet.exe ^
+/p:RequireRestoreConsent=false ^
+/p:Configuration=Release ^
+/p:Platform=AnyCPU ^
+/p:Major=12 ^
+/p:Minor=3 ^
+/p:Revision=0 ^
+/p:AssemblyInformationalVersion="See https://github.com/versionone/VersionOne.SDK.NET.ObjectModel/wiki" ^
+/p:AssemblyCopyright="Copyright 2012, VersionOne, Inc. Licensed under modified BSD." ^
+/p:CompanyName="VersionOne, Inc" ^
+/p:AssemblyProduct="VersionOne.SDK.ObjectModel" ^
+/p:AssemblyTitle="VersionOne SDK Object Model" ^
+/p:AssemblyDescription="VersionOne SDK .NET Object Model Release Build" ^
+/p:SignAssembly=%SIGN_ASSEMBLY% ^
+/p:AssemblyOriginatorKeyFile=%SIGNING_KEY%
